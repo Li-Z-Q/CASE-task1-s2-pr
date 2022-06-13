4f9a3ee37e7c733e6271e6c0d1b0a04581f4c5fe
2305932025@qq.com <2305932025@qq.com>
2022-06-13 15:56:22 +0800

    solve bash bug

--- run_train.sh
+++ run_train.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
-
+\
 source activate
 conda activate py38
-
+\
 export method=bert
-
+\
 export use_cache=False
 export cache_dir=./data/subtask2-sentence/cache_bert_base
 export train_data_path=./data/subtask2-sentence/en-train-train.json
 export dev_data_path=./data/subtask2-sentence/en-train-dev.json
-
+\
 export pretrained_model=bert-base-uncased
-
+\
 export gpu_id=3
 export random_seed=1234
-
+\
 export max_seq_len=64
 export dev_batch_size=16
 export train_batch_size=32
-
+\
 export lr=1e-5
 export epochs=3
 export weight_decay=1e-4
-
+\
 export warm_ratio=1.0
 export max_grad_norm=99999
 export gradient_accumulation_steps=1
-
+\
 nohup python -u train.py \
   -method ${method} \
   \
